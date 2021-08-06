fefea5cee16235c7c56f04327cf0fd31c2d76379
csicsi2000 <csicsi0707@gmail.com>
2021-08-06 10:45:37 +0200

    added class

---
+++ WindowsFormsApp autósiskola/DataType/GoogleUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_autósiskola.DataType
+{
+    class GoogleUser
+    {
+        public string UserName { get; set; }
+        public string Code { get; set; }
+        public string sheetAccess { get; set; }
+        public GoogleUser(string userName, string code, string url)
+        {
+            UserName = userName;
+            Code = code;
+
+            string[] urlSplit = url.Split('/');
+            sheetAccess = urlSplit[5];
+        }
+    }
+}
